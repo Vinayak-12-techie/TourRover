e3e980bd0fc5762fbd9a0997142f9cf70ccdf430
Vinayak <vinayak122001@gmail.com>
2023-09-11 17:53:25 +0530

    Changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,34 @@
 const Tour = require('./../models/tourModel');
+const multer = require('multer');
+const sharp = require('sharp');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an Image!! Plaease upload only image', 400), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+};
+
+// upload.array('images', 5);
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
